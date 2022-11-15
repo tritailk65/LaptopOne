ee039e6d0013ff7f795cc96f5f6e9774c419a586
Kiến Huy Lý <kienhuy1411@gmail.com>
2022-11-15 20:04:03 +0700

    render ProjectDetails

---
+++ main.js
@@ -0,0 +1,219 @@
+function SignUpOpen() {
+    var textHTLM = '<div class="sign-up-form"> \
+    <div onclick="SignUpClose()" class="sign-up_close-btn btn">x</div>\
+    <div class="sign-up-form_label">ĐĂNG NHẬP VÀO TÀI KHOẢN</div>\
+    <div class="form-group">\
+        <label for="uname" class="form-label">Tên đăng nhập hoặc Email</label>\
+        <br>\
+        <input class="form-input" id="uname" name="uname" type="text" placeholder="Nhập vào tên tài khoản hoặc Email">\
+        <br>\
+        <span class="form-message"></span>\
+    </div>\
+    <div class="form-group">\
+        <label for="pwd" class="form-label">Mật khẩu</label>\
+        <br>\
+        <input class="form-input" id="pwd" name="pwd" type="password" placeholder="Nhập vào mật khẩu">\
+        <br>\
+        <span class="form-message"></span>\
+    </div>\
+    <div class="form-group">\
+        <input id="checkbox" name="checkbox" type="checkbox">Remember me\
+        <a href="" class="forgot-pwd">Forgot Password?</a>\
+    </div>\
+    <div class="sign-up_control">\
+        <div class="sign-up_btn btn">Đăng nhập</div>\
+        <div class="register">\
+            <p class="register_txt">Chưa có tài khoản</p>\
+            <i class="fa-solid fa-arrow-right"></i>\
+            <div onclick="RegisterOpen()" class="rigister_btn btn">Đăng ký</div>\
+        </div>\
+    </div>\
+</div>';
+    document.getElementById('modal').innerHTML=textHTLM;
+    document.querySelector('.modal').style.display = "block";
+    document.querySelector('.sign-up-form').style.display = "block";
+}
+
+function SignUpClose() {
+    document.querySelector('.modal').style.display = 'none';
+}
+
+function RegisterOpen() {
+    var textHTLM = '<div class="register-form">\
+    <div onclick="RegisterClose()" class="sign-up_close-btn btn">x</div>\
+    <div class="register_label">ĐĂNG KÝ TÀI KHOẢN MỚI</div>\
+    <div class="form-group">\
+        <label for="uname" class="form-label">Tên đăng nhập hoặc Email</label>\
+        <br>\
+        <input class="form-input" id="uname" name="uname" type="text" placeholder="Nhập vào tên tài khoản hoặc Email">\
+        <br>\
+        <span class="form-message"></span>\
+    </div>\
+    <div class="form-group">\
+        <label for="pwd" class="form-label">Mật khẩu</label>\
+        <br>\
+        <input class="form-input" id="pwd" name="pwd" type="password" placeholder="Nhập mật khẩu">\
+        <br>\
+        <span class="form-message"></span>\
+    </div>\
+    <div class="form-group">\
+        <label for="pwd" class="form-label">Nhập lại mật khẩu</label>\
+        <br>\
+        <input class="form-input" id="pwd" name="pwd" type="password" placeholder="Nhập lại mật khẩu">\
+        <br>\
+        <span class="form-message"></span>\
+    </div>\
+    <div class="form-group">\
+        <p class="register_privacy">Bằng việc đăng ký, bạn đã đồng ý với <a href="">Điều khoản dịch vụ</a> & <a href="">Chính sách bảo mật</a></p>\
+    </div>\
+    <div class="sign-up_control">\
+        <div class="sign-up_btn btn">Đăng ký</div>\
+        <div class="register">\
+            <p class="register_txt">Đã có tài khoản</p>\
+            <i class="fa-solid fa-arrow-right"></i>\
+            <div onclick="SignUpOpenFromRegister()" class="rigister_btn btn">Đăng nhập</div>\
+        </div>\
+    </div>\
+    </div>';
+    document.getElementById('modal').innerHTML=textHTLM;
+    document.querySelector('.register-form').style.display='block';
+}
+
+function RegisterClose(){
+    document.querySelector('.register-form').style.display='none';
+    document.querySelector('.modal').style.display='none';
+}
+
+function SignUpOpenFromRegister(){
+    SignUpOpen();
+}
+
+var lapOfficeApi = 'http://localhost:3000/Product_LaptopVanPhong';
+var lapGamingApi = 'http://localhost:3000/Product_LaptopGaming';
+var keyboardApi = 'http://localhost:3000/Product_Keyboard';
+var mouseApi = 'http://localhost:3000/Product_Mouses';
+
+start()
+
+function start() {
+    getProductLapOffice(renderProductLapOffice)
+    getProductLapGaming(renderProductLapGaming)
+    getProductKeyboards(renderKeyboards)
+    getProductMouses(renderMouses)
+}
+
+function getProductLapOffice(callback) {
+    fetch(lapOfficeApi)
+        .then(function(response){
+            return response.json();
+        })
+        .then(callback)
+}
+
+function renderProductLapOffice(Products){
+    var listProduct = document.getElementById('LapVanPhong_Recommend');
+    var htmls = Products.map(function (product) {
+        return `<div class="product-detail" value="${product.id}" origin="Office" onclick="UpLoad(this)">
+            <div class="new-product-img">
+                <img class="new-product-thumbnail"
+                src="${product.img}" alt="" />
+            </div>
+            <div class="new-product-percent">${product.new_productt_percent}</div>               
+            <h2 class="new-product-name">${product.name}</h2>
+            <del>${product.old_price}</del><br/>
+            <span class="new-product-sale">${product.new_price}</span>
+            </div>`;
+    });
+    listProduct.innerHTML = htmls.join("");
+}
+
+function getProductLapGaming(callback) {
+    fetch(lapGamingApi)
+        .then(function(response){
+            return response.json();
+        })
+        .then(callback)
+}
+
+function renderProductLapGaming(Products){
+    var listProduct = document.getElementById('LapGaming_Recommend');
+    var htmls = Products.map(function (product) {
+        return `<div class="product-detail" value="${product.id}" origin="Gaming" onclick="UpLoad(this)">
+            <div class="new-product-img">
+                <img class="new-product-thumbnail"
+                src="${product.img}" alt="" />
+            </div>
+            <div class="new-product-percent">${product.new_productt_percent}</div>               
+            <h2 class="new-product-name">${product.name}</h2>
+            <del>${product.old_price}</del><br/>
+            <span class="new-product-sale">${product.new_price}</span>
+            </div>`;
+    });
+    listProduct.innerHTML = htmls.join("");
+}
+
+function getProductKeyboards(callback) {
+    fetch(keyboardApi)
+        .then(function(response){
+            return response.json();
+        })
+        .then(callback)
+}
+
+function renderKeyboards(Products){
+    var listProduct = document.getElementById('Keyboard_Recommend');
+    var htmls = Products.map(function (product) {
+        return `<div class="product-detail" value="${product.id}" origin="Keyboard" onclick="UpLoad(this)">
+            <div class="new-product-img">
+                <img class="new-product-thumbnail"
+                src="${product.img}" alt="" />
+            </div>
+            <div class="new-product-percent">${product.new_productt_percent}</div>               
+            <h2 class="new-product-name">${product.name}</h2>
+            <del>${product.old_price}</del><br/>
+            <span class="new-product-sale">${product.new_price}</span>
+            </div>`;
+    });
+    listProduct.innerHTML = htmls.join("");
+}
+
+function getProductMouses(callback) {
+    fetch(mouseApi)
+        .then(function(response){
+            return response.json();
+        })
+        .then(callback)
+}
+
+function renderMouses(Products){
+    var listProduct = document.getElementById('Mouse_Recommend');
+    var htmls = Products.map(function (product) {
+        return `<div class="product-detail" value="${product.id}" origin="Mouse" onclick="UpLoad(this)">
+            <div class="new-product-img">
+                <img class="new-product-thumbnail"
+                src="${product.img}" alt="" />
+            </div>
+            <div class="new-product-percent">${product.new_productt_percent}</div>               
+            <h2 class="new-product-name">${product.name}</h2>
+            <del>${product.old_price}</del><br/>
+            <span class="new-product-sale">${product.new_price}</span>
+            </div>`;
+    });
+    listProduct.innerHTML = htmls.join("");
+}
+
+function containerHomeClose(){
+    var htmls ='<div class="productdetails">\
+                <div class="productdetails__header">\
+                    <div class="productdetails__header--information">\
+                        <div class="productdetails__header--information--image" id="product--image"></div>\
+                        <div class="productdetails__header--information--content" id="product--content"></div>\
+                    </div>\
+                </div>\
+                <div class="productdetails__detailss" id="productdetail"></div>\
+            </div>';
+    document.getElementById('content-home').style.display='none';
+    document.getElementById('content-productdetails').style.display='block'
+}
+
+
---
+++ upload.js
@@ -0,0 +1,126 @@
+const url="http://localhost:3000/Product_LaptopVanPhong"
+function UpLoad(product){
+    var upload=document.getElementById('content-home')
+    var id=product.getAttribute("value")
+    fetch(url)
+    .then(response => response.json())
+    .then(data =>{
+        data.forEach(item => {
+            if(item.id==id){
+    upload.innerHTML=`<div class="productdetails">
+    <div class="productdetails__header">
+        <div class="productdetails__header--information">
+            <div class="productdetails__header--information--image" id="product--image">
+                <div class="productdetails__header--information--image--item" id="image">
+                    <div class="productdetails__header--information--image--item--image" id="image-1" style="background-image: url('${item.img}') ;"></div>
+                    <div class="productdetails__header--information--image--item--image" id="image-2"></div>
+                    <div class="productdetails__header--information--image--item--image" id="image-3"></div>
+                    <div class="productdetails__header--information--image--item--image" id="image-4"></div>
+                    <div class="productdetails__header--information--image--item--image" id="image-5"></div>
+                    <div class="productdetails__header--information--image--item--image" id="image-6"></div>
+                </div>
+                <button class="productdetails__header--information--image--previous" onclick="PreviousImage()"><</button>
+                <button class="productdetails__header--information--image--next" onclick="NextImage()">></button>
+            </div>
+            <div class="productdetails__header--information--content" id="product--content">
+                <div class="productdetails__header--information--content--name" id="product-name">${item.name}</div>
+                <div class="productdetails__header--information--content--info">
+                    <span class="productdetails__header--information--content--info--details1">Thông tin chung: </span><br>
+                    <span class="productdetails__header--information--content--info--details2">Bảo hành</span><br>
+                    <span class="productdetails__header--information--content--info--details2">Hãng</span><br>
+                    <span class="productdetails__header--information--content--info--details2">Window bản quyền</span>
+                </div>
+                <div class="productdetails__header--information--content--price" id="product-price"><del>${item.old_price}</del><br>${item.new_price}</div>
+                <div class="productdetails__header--information--content--order" onclick="OrderProduct()">Đặt hàng</div>
+                </div>
+                </div>
+            </div>
+            <div class="productdetails__details" id="productdetail">Thông số kỹ thuật
+                <table class="productdetails__details--table" cellspacing="3" cellpadding="10" border="10" bordercolor="black" id="product--table">
+                    <tr>
+                        <td>CPU:</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Ram:</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Ổ cứng:</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Card đồ họa:</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Màn hình:</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Cổng giao tiếp:</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Audio:</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Bàn phím:</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Đọc thẻ nhớ:</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Bluetooth:</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Webcam:</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Hệ điều hành:</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Pin:</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Trọng lượng:</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Màu sắc:</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Kích thước:</td>
+                        <td></td>
+                    </tr>
+                </table>
+            </div>
+        </div>`
+    }
+        });
+    });
+}
+function upload(){
+    const str =
+    "<div class='productdetails'>\
+    <div class='productdetails__header'>\
+    <div class='productdetails__header--information'>\
+    <div class='productdetails__header--information--image' id='product--image'>\
+    </div>\
+    <div class='productdetails__header--information--content' id='product--content'>\
+    </div>\
+    </div>\
+    </div>\
+    <div class='productdetails__details' id='productdetail'>\
+    </div>\
+    </div>";
+    document.getElementById('uploaddata').innerHTML=str;
+}
